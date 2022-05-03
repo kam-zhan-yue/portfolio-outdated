3724e0479f92d36fc50640828ffb1e56dc4d922d
Alexander Kam <kamzhanyue@gmail.com>
2022-05-03 23:04:20 +1000

    Fix Timeline Margins and Add Media Queries

--- assets/js/progress-bar.js
+++ assets/js/progress-bar.js
@@ -2,17 +2,39 @@ jQuery(document).ready(function($) {
 
   initProgressBars();
 
+  var lastScrollTop = 0;
   var animated = 0;
+  var classRemoved = 0;
   var offset = 0;
   var scrollContainer = $(".scroll-container");
   scrollContainer.scroll(function () {
+    // var skillsPosition = $("skills-section").position();
+    // var skillsTop = skillsPosition.top;
     var skillsSection = $("#skills-section").height();
     var viewPortSize = $(scrollContainer).height();
     var scrollTop = scrollContainer.scrollTop();
-    if(scrollTop >= skillsSection + offset && animated == 0) {
-      animated = 1;
-      animateProgressBars();
+
+    downScroll = scrollTop > lastScrollTop;
+    lastScrollTop = scrollTop;
+
+    console.log("Downscroll is: "+downScroll);
+    // console.log("Scroll Top: "+scrollTop+"\nSkills Section: "+skillsSection+"\nSkills Top"+skillsTop);
+
+    if(downScroll && scrollTop >= skillsSection + offset) {
+      if(animated == 0)
+      {
+        animated = 1;
+        animateProgressBars();
+      }
+      if(classRemoved == 0)
+      {
+        classRemoved = 1;
+        console.log("Removed y-mandatory");
+        $("#scroll-main").removeClass("y-mandatory");
+      }
     }
+
+
   });
 
   $("#skill-unity").click(function(){
